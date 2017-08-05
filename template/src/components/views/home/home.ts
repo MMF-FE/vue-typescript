fa4eb428862c6084db7b9580a0a05a36d4fbad2f
Allenice <994298628@qq.com>
2017-08-05 08:10:03 -0400

    fix #6

--- template/src/components/views/home/home.ts
+++ template/src/components/views/home/home.ts
@@ -1,6 +1,5 @@
 import Vue from 'components/base'
 import { Component, Watch, Prop } from 'vue-property-decorator'
-import { State, Mutation, Getter, Action} from 'vuex-class'
 import template from './home.vue'
 import Hello from 'components/tags/hello'
 
